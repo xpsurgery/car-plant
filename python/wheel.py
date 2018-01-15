3fd392750d19f533c6a26a3d82b8dbde5c3d3331
Kevin Rutherford <kevin@rutherford-software.com>
2018-01-15 16:51:06 +0000

    The LOG is a listener on the Wheels

--- python/wheel.py
+++ python/wheel.py
@@ -1,15 +1,15 @@
 class Wheel:
 
-    def __init__(self, name, log):
+    def __init__(self, name, listener):
         self.name = name
-        self.log = log
+        self.listener = listener
 
     def turnLeft(self, degrees):
-        self.log.append(self.name + " turning left " + str(degrees) + " degrees\n")
+        self.listener.append(self.name + " turning left " + str(degrees) + " degrees\n")
 
     def turnRight(self, degrees):
-        self.log.append(self.name + " turning right " + str(degrees) + " degrees\n")
+        self.listener.append(self.name + " turning right " + str(degrees) + " degrees\n")
 
     def rotate(self, kph):
-        self.log.append(self.name + " accelerating " + str(kph) + " kph\n")
+        self.listener.append(self.name + " accelerating " + str(kph) + " kph\n")
 
